312bd5eaf793bf18adf5ba1acb31d360a79ebfa7
Nourhan Mahmoud Sarhan <92642003+Nourhasarhan@users.noreply.github.com>
2024-05-19 16:10:38 +0300

    FibonacciGenerator.py

--- FibonacciGenerator.py
+++ FibonacciGenerator.py
@@ -1,15 +1,10 @@
-
 def generate_fibonacci(n):
-    
-    seq = [0,1]        # initial array with 0 and 1 #
-    
-    while len(seq) < n:      #loop till       
-        
-     seq.append(seq[-1]+ seq[-2])    #add an element at the end of the array by adding the prev two elemnt
-        
-    return seq[:n]
+    seq = [0, 1]  # initial array with 0 and 1
+    while len(seq) < n:  # loop until the sequence has n elements
+        seq.append(seq[-1] + seq[-2])  # add the next element in the sequence
+    return seq[:n]  # return the first n elements of the sequence
 
-n = 10
+# Take input from the user
+n = int(input("Enter the number of Fibonacci numbers to generate: "))
 fib_number = generate_fibonacci(n)
 print(fib_number)
-    
